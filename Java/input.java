c6a36a7c85fcd74c8fb38c1e897dbb5e5d299f82
PRANAV VERMA <47380738+pranav12verma@users.noreply.github.com>
2021-10-23 00:17:16 +0530

    Create input.java

---
+++ Java/input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class input{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter age");
+    int n = sc.nextInt();
+    System.out.println("Age is :",n);
+  }
+}
