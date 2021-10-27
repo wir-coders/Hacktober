6b1d8274015da29ed20086d2ca80bbc52ef7ea45
MANSI KANHEY <47391058+mansikanhey@users.noreply.github.com>
2021-10-27 10:42:44 +0530

    Create Sun.java

---
+++ Java/Sun.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Sun{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int sun = sc.nextInt();
+    System.out.println("A number from sun"+sun);
+  }
+}
